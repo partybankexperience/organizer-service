77fb0de87129e7a3ddc8e294428f9be0ff30c66e
Femi Oladeji <oladejifemi00@gmail.com>
2024-03-08 13:55:51 +0100

    refactored repositories

--- app/repositories/Repository.go
+++ app/repositories/Repository.go
@@ -1,5 +1,11 @@
 package repositories
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
 type Repository[T, U any] interface {
 	Save(t *T) *T
 	FindById(id U) *T
@@ -10,8 +16,10 @@ type Repository[T, U any] interface {
 type RepositoryImpl[T, U any] struct {
 }
 
-func (r *RepositoryImpl[T, U]) Save(t *T) *T {
+var db = connect()
 
+func (r *RepositoryImpl[T, U]) Save(t *T) *T {
+	
 	return nil
 }
 
@@ -25,4 +33,15 @@ func (r *RepositoryImpl[T, U]) FindAll() []*T {
 
 func (r *RepositoryImpl[T, U]) DeleteById(id U) {
 
+}
+
+func connect() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=rave port=5432 sslmode=disable TimeZone=Africa/Lagos"
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true}), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
