76e2b9161e745ad8267bb61ff5bc0f9dea2d8f22
Femi Oladeji <oladejifemi00@gmail.com>
2024-03-17 13:29:27 +0100

    modified AuthController

--- app/security/AuthController.go
+++ app/security/AuthController.go
@@ -4,7 +4,6 @@ import (
 	"github.com/djfemz/rave/app/services"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
-	"log"
 	"net/http"
 )
 
@@ -20,20 +19,22 @@ func LoginHandler(ctx *gin.Context) {
 	var loginResponse = make(map[string]string)
 	var err error
 	err = ctx.BindJSON(&loginRequest)
-	log.Println(loginRequest)
 	if err != nil {
-		loginResponse["error"] = err.Error()
-		ctx.IndentedJSON(http.StatusBadRequest, loginResponse)
-		return
+		handleError(ctx, err, loginResponse)
 	}
 	org := organizationService.GetByUsername(loginRequest.Username)
 	err = bcrypt.CompareHashAndPassword([]byte(org.Password), []byte(loginRequest.Password))
 	if err != nil {
-		loginResponse["error"] = err.Error()
-		ctx.IndentedJSON(http.StatusBadRequest, loginResponse)
-		return
+		handleError(ctx, err, loginResponse)
 	}
 	token, err := GenerateAccessToken(org)
 	loginResponse["access_token"] = token
 	ctx.JSON(http.StatusOK, loginResponse)
+}
+
+func handleError(ctx *gin.Context, err error, loginResponse map[string]string) {
+	if err != nil {
+		loginResponse["error"] = err.Error()
+		ctx.IndentedJSON(http.StatusBadRequest, loginResponse)
+	}
 }
