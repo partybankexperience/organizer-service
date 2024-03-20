bcf00cfa09e79ecc7af6ba0a019f1f3583de2d57
Femi Oladeji <oladejifemi00@gmail.com>
2024-03-20 15:15:57 +0100

    Added config file and modified mail service

--- app/dtos/response/Responses.go
+++ app/dtos/response/Responses.go
@@ -1,5 +1,9 @@
 package dtos
 
+const (
+	MAIL_SENDING_SUCCESS_MESSAGE = "mail sent successfully"
+)
+
 type LoginResponse struct {
 }
 
--- app/services/MailService.go
+++ app/services/MailService.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	request "github.com/djfemz/rave/app/dtos/request"
+	response "github.com/djfemz/rave/app/dtos/response"
 	"github.com/djfemz/rave/config"
 	"log"
 	"net/http"
@@ -25,22 +26,22 @@ type RaveMailService struct {
 
 func (raveMailService *RaveMailService) Send(emailRequest *request.EmailNotificationRequest) string {
 	jsonData, _ := json.Marshal(emailRequest)
-
 	appConfig := config.LoadConfigFile()
 	req, err := http.NewRequest(http.MethodPost, appConfig.MAIL_API_URL, bytes.NewReader(jsonData))
+	if err != nil {
+		log.Fatal("Error Creating request", err)
+	}
+	addHeadersTo(req, appConfig)
 
+	client := &http.Client{}
+	if _, err = client.Do(req); err != nil {
+		log.Fatal("Error sending mail: ", err)
+	}
+	return response.MAIL_SENDING_SUCCESS_MESSAGE
+}
+
+func addHeadersTo(req *http.Request, appConfig *config.EnvConfig) {
 	req.Header.Add(API_KEY_VALUE, appConfig.MAIL_API_KEY)
 	req.Header.Add(CONTENT_TYPE_KEY, APPLICATION_JSON_VALUE)
 	req.Header.Add(ACCEPT_HEADER_KEY, APPLICATION_JSON_VALUE)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client := &http.Client{}
-	res, err := client.Do(req)
-	log.Println(res)
-	if err != nil {
-		log.Fatal("Error sending mail: ", err)
-	}
-	log.Println("response: ", res)
-	return "Mail sent successfully"
 }
