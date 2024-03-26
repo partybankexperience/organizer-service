8ff079378b6ac3d71cd429a605a6a2d4ced1f0bc
Femi Oladeji <oladejifemi00@gmail.com>
2024-03-26 15:23:03 +0100

    fixed edit event route

--- rave-app/controllers/EventController.go
+++ rave-app/controllers/EventController.go
@@ -21,23 +21,42 @@ func NewEventController() *EventController {
 
 func (eventController *EventController) EditEvent(ctx *gin.Context) {
 	updateEventRequest := &request.UpdateEventRequest{}
-	eventId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	eventId, err := extractIdFrom(ctx)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest,
-			&response.RaveResponse[string]{Data: err.Error()})
+		handleError(ctx, err)
 		return
 	}
 	err = ctx.BindJSON(updateEventRequest)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest,
-			&response.RaveResponse[string]{Data: err.Error()})
+		handleError(ctx, err)
 		return
 	}
 	updateEventResponse, err := eventController.EventService.UpdateEventInformation(eventId, updateEventRequest)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest,
-			&response.RaveResponse[string]{Data: err.Error()})
+		handleError(ctx, err)
 		return
 	}
 	ctx.JSON(http.StatusOK, updateEventResponse)
+}
+
+func handleError(ctx *gin.Context, err error) {
+	ctx.AbortWithStatusJSON(http.StatusBadRequest,
+		&response.RaveResponse[string]{Data: err.Error()})
+	return
+}
+
+func extractIdFrom(ctx *gin.Context) (uint64, error) {
+	return strconv.ParseUint(ctx.Param("id"), 10, 64)
+}
+
+func (eventController *EventController) GetEventById(ctx *gin.Context) {
+	id, err := extractIdFrom(ctx)
+	if err != nil {
+		handleError(ctx, err)
+	}
+	event, err := eventController.EventService.GetById(id)
+	if err != nil {
+		handleError(ctx, err)
+	}
+	ctx.JSON(http.StatusOK, event)
 }
