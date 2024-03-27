087c6ef3088ebd1e225c892f39286f7f3565ac27
Femi Oladeji <oladejifemi00@gmail.com>
2024-03-27 08:43:45 +0100

    modified EventService.go - added endpoint to get all events for an organizer

--- main.go
+++ main.go
@@ -3,23 +3,22 @@ package main
 import (
 	security "github.com/djfemz/rave/rave-app/security/controllers"
 	"github.com/djfemz/rave/rave-app/security/middlewares"
-	"github.com/joho/godotenv"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error starting server: ", err)
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Println("Error starting server: ", err)
+	//}
 	router := gin.Default()
 	middlewares.Routers(router)
 	authController := security.NewAuthController()
 	router.POST("/auth/login", authController.AuthHandler)
 	router.GET("/auth/validate-otp", authController.ValidateOtp)
-	err = router.Run(":8082")
+	err := router.Run(":8082")
 	if err != nil {
 		log.Println("Error starting server: ", err)
 	}
