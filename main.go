170231fef19cbde6d96a330640325ed76d6c760d
Femi Oladeji <oladejifemi00@gmail.com>
2024-05-15 13:38:23 +0100

    added cors config

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	security "github.com/djfemz/rave/rave-app/security/controllers"
 	"github.com/djfemz/rave/rave-app/security/middlewares"
+	"github.com/gin-contrib/cors"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +18,7 @@ func main() {
 	}
 	router := gin.Default()
 	middlewares.Routers(router)
+	router.Use(cors.New(configureCors()))
 	authController := security.NewAuthController()
 	router.POST("/auth/login", authController.AuthHandler)
 	router.GET("/auth/validate-otp", authController.ValidateOtp)
@@ -23,4 +26,13 @@ func main() {
 	if err != nil {
 		log.Println("Error starting server: ", err)
 	}
+}
+
+func configureCors() cors.Config {
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowCredentials = true
+	config.AllowMethods = []string{http.MethodOptions,
+		http.MethodPost, http.MethodOptions, http.MethodPost, http.MethodGet}
+	return config
 }
