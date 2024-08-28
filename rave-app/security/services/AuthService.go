cc7574032f4a88fdc131a4f2d05ffdf566f0b40b
Femi Oladeji <oladejifemi00@gmail.com>
2024-08-28 11:25:35 +0100

    refactored validate otp

--- rave-app/security/services/AuthService.go
+++ rave-app/security/services/AuthService.go
@@ -56,6 +56,7 @@ func (authenticationService *AuthService) ValidateOtp(otp string) (*response.Rav
 		return nil, err
 	}
 	orgResponse := mapOrgToOrgResponse(org)
+	log.Println("orgResponse: ", orgResponse)
 	token, err := security.GenerateAccessTokenFor(org)
 	if err != nil {
 		return nil, err
