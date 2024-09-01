71834b3f62d5a44b26a4a284ef6ef2c1fdd292a7
Femi Oladeji <oladejifemi00@gmail.com>
2024-09-01 15:05:39 +0100

    added discover events endpoint

--- docs/docs.go
+++ docs/docs.go
@@ -1080,7 +1080,7 @@ var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "rave.onrender.com",
 	BasePath:         "",
-	Schemes:          []string{"http"},
+	Schemes:          []string{"https"},
 	Title:            "Partybank Organizer Service",
 	Description:      "Partybank Organizer Service.",
 	InfoInstanceName: "swagger",
