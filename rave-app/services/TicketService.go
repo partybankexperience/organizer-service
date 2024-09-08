e392f5da0c89bbb83bf5f0750a9a49081642c72e
Femi Oladeji <oladejifemi00@gmail.com>
2024-09-08 14:05:55 +0100

    fixed structure of TicketMessage object

--- rave-app/services/TicketService.go
+++ rave-app/services/TicketService.go
@@ -139,6 +139,7 @@ func sendNewTicketMessageFor(event *models.Event) {
 	}
 	req, err := http.NewRequest(http.MethodPost, os.Getenv("TICKET_SERVICE_URL"), bytes.NewReader(body))
 	req.Header.Add("Content-Type", APPLICATION_JSON_VALUE)
+	log.Println("request data: ", req.Body)
 	client := &http.Client{}
 	_, err = client.Do(req)
 	if err != nil {
