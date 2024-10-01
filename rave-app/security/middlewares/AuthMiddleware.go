5e02c4234192818604fe635e56c5cbe2e1c8f323
Femi Oladeji <oladejifemi00@gmail.com>
2024-10-01 23:42:27 +0100

    updated cors configurer

--- rave-app/security/middlewares/AuthMiddleware.go
+++ rave-app/security/middlewares/AuthMiddleware.go
@@ -88,7 +88,7 @@ func configureCors() cors.Config {
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
 	config.AllowCredentials = true
-	config.AllowHeaders = []string{utils.AUTHORIZATION}
+	config.AllowHeaders = []string{utils.AUTHORIZATION, "Content-Type"}
 	config.AllowMethods = []string{http.MethodOptions,
 		http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodGet}
 	return config
