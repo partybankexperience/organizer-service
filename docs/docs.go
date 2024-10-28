6745f2ffbe42faaf9241d5d4edbbe4f1e3fda205
Femi Oladeji <oladejifemi00@gmail.com>
2024-10-28 13:33:32 +0100

    fixed bug in edit events where deleted tickets were returned by endpoint

--- docs/docs.go
+++ docs/docs.go
@@ -1700,7 +1700,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "partybank-organizer-269c8057a65f.herokuapp.",
+	Host:             "partybank-organizer-269c8057a65f.herokuapp.com",
 	BasePath:         "",
 	Schemes:          []string{"https"},
 	Title:            "Partybank Organizer Service",
