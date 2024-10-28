144238f814c810aeb19c1c4e39152fb7b40a9957
Femi Oladeji <oladejifemi00@gmail.com>
2024-10-28 13:26:35 +0100

    fixed bug in edit events where deleted tickets were returned by endpoint

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func init() {
 // @contact.email  unavailable
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-// @host partybank-organizer-269c8057a65f.herokuapp.
+// @host partybank-organizer-269c8057a65f.herokuapp.com
 // @schemes https
 // @securityDefinitions.apikey Bearer
 // @in header
