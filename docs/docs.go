8512d35baa2d2b8902a8b63f9f028881a09178e6
djfemz <oladejifemi00@gmail.com>
2024-12-07 23:20:40 +0100

    added cron job to remove past events

--- docs/docs.go
+++ docs/docs.go
@@ -1763,7 +1763,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "partybank-organizer-269c8057a65f.herokuapp.com",
+	Host:             "partybank-organizer.onrender.com",
 	BasePath:         "",
 	Schemes:          []string{"https"},
 	Title:            "Partybank Organizer Service",
