1824441d2a908b2f7426b4c328fb336e41d48a7a
djfemz <oladejifemi00@gmail.com>
2024-12-19 20:17:58 +0100

    added auth to payment service

--- test/app_utils_test.go
+++ test/app_utils_test.go
@@ -1,5 +1,12 @@
 package test
 
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"mime/multipart"
+)
+
 //
 //import (
 //	"github.com/djfemz/rave/partybank-app/models"
@@ -20,3 +27,60 @@ package test
 //	status := utils.IsTicketSaleEndedFor(ticket)
 //	log.Println(status)
 //}
+
+
+package main
+
+import (
+"errors"
+"io"
+"log"
+"mime/multipart"
+
+"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.POST("/upload", UploadImage)
+	router.GET("/image/:id", GetImage)
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalln("error starting server: ", err)
+	}
+}
+
+var images = make(map[string]string)
+
+func UploadImage(ctx *gin.Context) {
+	file, _, err := ctx.Request.FormFile("file")
+	if err != nil {
+		ctx.AbortWithStatusJSON(400, errors.New("file not present in request"))
+		return
+	}
+	defer func(file multipart.File) {
+		err := file.Close()
+		if err != nil {
+			log.Println("error: ", err)
+		}
+	}(file)
+
+	bs, err := io.ReadAll(file)
+	if err != nil {
+		ctx.AbortWithStatusJSON(400, errors.New("could not read from file"))
+		return
+	}
+	images["abc"] = string(bs)
+	ctx.JSON(200, map[string]string{"url": "http://localhost:8080/image/abc"})
+}
+
+func GetImage(ctx *gin.Context) {
+	imageId := ctx.Param("id")
+	if imageId == "abc" {
+		image := images["abc"]
+		bs := []byte(image)
+		ctx.Data(200, "image/png", bs)
+		return
+	}
+	ctx.AbortWithStatusJSON(400, errors.New("not found"))
+}
+
