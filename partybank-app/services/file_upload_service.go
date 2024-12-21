baac10ce5ea2119656b330568f3496e8fdf298fd
djfemz <oladejifemi00@gmail.com>
2024-12-21 14:17:17 +0100

    added add image as byte slice feature

--- partybank-app/services/file_upload_service.go
+++ partybank-app/services/file_upload_service.go
@@ -14,6 +14,7 @@ import (
 
 type FileUploadService interface {
 	UploadImage(file multipart.File) (*dtos.ImageUploadResponse, error)
+	UploadImageContent(file []byte) (*dtos.ImageUploadResponse, error)
 	GetImage(imageId string) ([]byte, error)
 }
 
@@ -28,24 +29,22 @@ func NewFileUploadService(imageService ImageService) *PartybankFileUploadService
 }
 
 func (partybankFileUploadService *PartybankFileUploadService) UploadImage(file multipart.File) (*dtos.ImageUploadResponse, error) {
-	bs, err := io.ReadAll(file)
+	imageContent, err := io.ReadAll(file)
 	if err != nil {
 		return nil, errors.New("failed to read from image file")
 	}
+	return partybankFileUploadService.UploadImageContent(imageContent)
+}
+
+func (partybankFileUploadService *PartybankFileUploadService) buildImageFrom(imageContent []byte) *models.Image {
 	imageId := utils.GenerateImageId(6)
-	//"https://organizer-service.onrender.com/"
 	imageUrl := os.Getenv("APPLICATION_BASE_URL") + "/api/v1/image/" + imageId
 	image := &models.Image{
 		ImageId: imageId,
-		Content: bs,
+		Content: imageContent,
 		Url:     imageUrl,
 	}
-	imageResponse, err := partybankFileUploadService.ImageService.AddImage(image)
-	if err != nil {
-		log.Println("Error: ", err)
-		return nil, errors.New("failed to add image")
-	}
-	return imageResponse, nil
+	return image
 }
 
 func (partybankFileUploadService *PartybankFileUploadService) GetImage(imageId string) ([]byte, error) {
@@ -55,4 +54,14 @@ func (partybankFileUploadService *PartybankFileUploadService) GetImage(imageId s
 		return nil, errors.New("failed to retrieve image")
 	}
 	return image.Content, nil
+}
+
+func (partybankFileUploadService *PartybankFileUploadService) UploadImageContent(file []byte) (*dtos.ImageUploadResponse, error) {
+	image := partybankFileUploadService.buildImageFrom(file)
+	res, err := partybankFileUploadService.AddImage(image)
+	if err != nil {
+		log.Println("Error: ", err)
+		return nil, errors.New("failed to add image")
+	}
+	return res, nil
 }
